93cecbd73cdcab6b0024c074d0ee19f58aec7e67
Frederic Duby <fcd1@columbia.edu>
2015-02-06 16:59:35 -0500

    Remove checkboxes (anonymous contribute and public) and simplify email
    Removed the checkboxes for contributing anonymously, and for make the
    contribution public: the checkbox are irrevelant since the "options"
    are now hard-coded -- anonymous contribution is not allowed, and all
    items can be made public.
    In the email, mention of a user account was removed, as well as the
    login link, and the link to the archive.

--- views/public/contribution/thankyou.php
+++ views/public/contribution/thankyou.php
@@ -1,10 +1,44 @@
-<?php echo head(); ?>
+<!DOCTYPE html>
+<html lang="<?php echo get_html_lang(); ?>">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+   <?php if ($description = option('description')): ?>
+      <meta name="description" content="<?php echo $description; ?>">
+	 <?php endif; ?>
+    <title>Contribute Item</title>
+    <!-- Plugin Stuff -->
+    <?php fire_plugin_hook('public_head', array('view'=>$this)); ?>
+
+    <!-- Stylesheets -->
+    <?php
+     //queue_css_file('screen');
+    echo head_css();
+    ?>
+
+    <?php
+    echo head_js();
+    ?>
+  </head>
+  <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
+    <?php
+      // fcd1, 7/25/13: one effect of the following call is the display of the admin bar
+      // when viewing the exhibition when one is logged into Omeka (as admin?).
+      // fcd1, 8/6/13: adding an empty admin-bar.php file in the common directory hides the admnin bar
+    ?>
+    <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
+    <?php
+    $imageURL = img("LoveInAction.png");
+if ($imageURL != "") {
+  echo '<img src="' . $imageURL . '" style="width: 50%"\>';
+ }
+?>
+<?php if ($imageURL) echo "</div>"; ?>
 <div id="primary">
 	<h1><?php echo __("Thank you for contributing!"); ?></h1>
-	<p><?php echo __("Your contribution will show up in the archive once an administrator approves it. Meanwhile, feel free to %s or %s ." , contribution_link_to_contribute(__('make another contribution')), "<a href='" . url('items/browse') . "'>" . __('browse the archive') . "</a>"); ?>
+	<p><?php echo __("Your contribution will show up in the collection once an administrator approves it.")?>
 	</p>
-	<?php if(get_option('contribution_simple') && !current_user()): ?>
-	<p><?php echo __("If you would like to interact with the site further, you can use an account that is ready for you. Visit %s, and request a new password for the email you used", "<a href='" . url('users/forgot-password') . "'>" . __('this page') . "</a>"); ?>
-	<?php endif; ?>
+	<p><?php echo __(contribution_link_to_contribute(__('Make another contribution.')))?>
+	</p>
 </div>
 <?php echo foot(); ?>
