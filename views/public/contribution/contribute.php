e44696ec80d5ff568b91a04b6cfdcb4f9cfa441c
Frederic Duby <fcd1@columbia.edu>
2015-02-18 13:51:03 -0500

    Aligned code, and added a missing <div>

--- views/public/contribution/contribute.php
+++ views/public/contribution/contribute.php
@@ -1,81 +1,83 @@
 <?php
-/**
- * @version $Id$
- * @license http://www.gnu.org/licenses/gpl-3.0.txt
- * @copyright Center for History and New Media, 2010
- * @package Contribution
- */
+  /**
+   * @version $Id$
+   * @license http://www.gnu.org/licenses/gpl-3.0.txt
+   * @copyright Center for History and New Media, 2010
+   * @package Contribution
+   */
 
-queue_js_file('contribution-public-form');
-$contributionPath = get_option('contribution_page_path');
-if(!$contributionPath) {
+  queue_js_file('contribution-public-form');
+  $contributionPath = get_option('contribution_page_path');
+  if(!$contributionPath) {
     $contributionPath = 'contribution';
-}
-queue_css_file('form');
+  }
 
-//load user profiles js and css if needed
-if(get_option('contribution_user_profile_type') && plugin_is_active('UserProfiles') ) {
+  queue_css_file('form');
+
+  //load user profiles js and css if needed
+  if(get_option('contribution_user_profile_type') && plugin_is_active('UserProfiles') ) {
     queue_js_file('admin-globals');
     queue_js_file('tiny_mce', 'javascripts/vendor/tiny_mce');
     queue_js_file('elements');
     queue_css_string("input.add-element {display: block}");
-}
+  }
 
-$head = array('title' => 'Contribute',
-              'bodyclass' => 'contribution');
+  $head = array('title' => 'Contribute',
+		'bodyclass' => 'contribution');
 ?>
+
 <!DOCTYPE html>
 <html lang="<?php echo get_html_lang(); ?>">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php if ($description = option('description')): ?>
-      <meta name="description" content="<?php echo $description; ?>">
-	 <?php endif; ?>
-    <title>Contribute Item</title>
-    <!-- Plugin Stuff -->
-    <?php fire_plugin_hook('public_head', array('view'=>$this)); ?>
+    <meta name="description" content="<?php echo $description; ?>">
+  <?php endif; ?>
+  <title>Contribute Item</title>
 
-    <!-- Stylesheets -->
-    <?php
-     //queue_css_file('screen');
+  <!-- Plugin Stuff -->
+  <?php fire_plugin_hook('public_head', array('view'=>$this)); ?>
+
+  <!-- Stylesheets -->
+  <?php
     echo head_css();
-    ?>
+  ?>
 
-    <?php
+  <?php
     echo head_js();
-    ?>
-  </head>
-  <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
-    <?php
-      // fcd1, 7/25/13: one effect of the following call is the display of the admin bar
-      // when viewing the exhibition when one is logged into Omeka (as admin?).
-      // fcd1, 8/6/13: adding an empty admin-bar.php file in the common directory hides the admnin bar
-    ?>
-    <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
-    <?php
-    $imageURL = img("LoveInAction.png");
-if ($imageURL != "") {
-  echo '<img src="' . $imageURL . '" style="width: 50%"\>';
- }
+  ?>
+</head>
+
+<?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
+<?php
+  // fcd1, 7/25/13: one effect of the following call is the display of the admin bar
+  // when viewing the exhibition when one is logged into Omeka (as admin?).
+  // fcd1, 8/6/13: adding an empty admin-bar.php file in the common directory hides the admnin bar
+?>
+<?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
+<?php
+  $imageURL = img("LoveInAction.png");
+  if ($imageURL != "") {
+    echo '<div class="contribute-banner">';
+    echo '<img src="' . $imageURL . '" style="width: 50%"\>';
+  }
 ?>
 <?php if ($imageURL) echo "</div>"; ?>
 <script type="text/javascript">
-// <![CDATA[
-enableContributionAjaxForm(<?php echo js_escape(url($contributionPath.'/type-form')); ?>);
-// ]]>
+  enableContributionAjaxForm(<?php echo js_escape(url($contributionPath.'/type-form')); ?>);
 </script>
 
 <div id="primary">
-<?php echo flash(); ?>
+  <?php echo flash(); ?>
     
     <h1><?php echo $head['title']; ?></h1>
 
     <?php if(!get_option('contribution_simple') && !$user = current_user()) :?>
-        <?php $session = new Zend_Session_Namespace;
-              $session->redirect = absolute_url();
-        ?>
-        <p>You must <a href='<?php echo url('guest-user/user/register'); ?>'>create an account</a> or <a href='<?php echo url('guest-user/user/login'); ?>'>log in</a> before contributing. You can still leave your identity to site visitors anonymous.</p>        
+      <?php $session = new Zend_Session_Namespace;
+        $session->redirect = absolute_url();
+      ?>
+      <p>You must <a href='<?php echo url('guest-user/user/register'); ?>'>create an account</a> or <a href='<?php echo url('guest-user/user/login'); ?>'>log in</a> before contributing. You can still leave your identity to site visitors anonymous.</p>        
     <?php else: ?>
         <form method="post" action="" enctype="multipart/form-data">
             <fieldset id="contribution-item-metadata">
